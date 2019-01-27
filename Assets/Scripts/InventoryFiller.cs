f3893050acfba06aeb531c5eb8664bc8e6c972ad
soksagim <44206344+soksagim@users.noreply.github.com>
2019-01-27 12:35:20 +0100

    WIP Inventory

---
+++ Assets/Scripts/InventoryFiller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InventoryFiller : MonoBehaviour
+{
+    private GameController gameController;
+    private GameObject gameControllerObject;
+    // Start is called before the first frame update
+    private Image[] imageList = new Image[5];
+    void Start()
+    {
+        this.gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+        gameController = gameControllerObject.GetComponent<GameController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for ()
+        {
+
+        }
+    }
+}
